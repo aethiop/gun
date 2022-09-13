df7cd7b13aa4e3cf87c3527546de6d2f59c5e510
Natnael Teferi <pinobirhanu3@gmail.com>
2022-09-12 19:10:14 -0500

    fs

--- examples/http.js
+++ examples/http.js
@@ -4,20 +4,21 @@
 	  return cluster.fork() && cluster.on('exit', function(){ cluster.fork(); require('../lib/crashed'); });
 	}
 
-	var fs = require('fs');
+	// var fs = require('fs');
 	var config = {
 		port: process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765,
 		peers: process.env.PEERS && process.env.PEERS.split(',') || []
 	};
 	var Gun = require('../'); // require('gun')
 
-	if(process.env.HTTPS_KEY){
-		config.key = fs.readFileSync(process.env.HTTPS_KEY);
-		config.cert = fs.readFileSync(process.env.HTTPS_CERT);
-		config.server = require('https').createServer(config, Gun.serve(__dirname));
-	} else {
+	// if(process.env.HTTPS_KEY){
+	// 	config.key = fs.readFileSync(process.env.HTTPS_KEY);
+	// 	config.cert = fs.readFileSync(process.env.HTTPS_CERT);
+	// 	config.server = require('https').createServer(config, Gun.serve(__dirname));
+	// }
+	// else {
 		config.server = require('http').createServer(Gun.serve(__dirname));
-	}
+	// }
 
 	var gun = Gun({
 		web: config.server.listen(config.port),
